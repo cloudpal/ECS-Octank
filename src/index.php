ddcd86a4480a2fc0997899c331e1d799d510ce3e
Rajamani <sonaraja@dca90483fb8d.ant.amazon.com>
2017-08-23 21:21:57 -0700

    changes to main Octank index.php

--- src/index.php
+++ src/index.php
@@ -15,9 +15,9 @@
     <body>
         <div class="container">
             <div class="hero-unit">
-                <h1>Simple PHP App</h1>
-                <h2>Congratulations</h2>
-                <p>Your PHP application is now running on a container in Amazon ECS.</p>
+                <h1>Octank - Continous Engineering Platform</h1>
+                <h2>By - Cheerleaders Team 4</h2>
+                <p>Octank e-commerce web  is now running on a container in Amazon ECS.</p>
                 <p>The container is running PHP version <?php echo phpversion(); ?>.</p>
                 <?php
                         $myfile = fopen("/var/www/my-vol/date", "r") or die("");
